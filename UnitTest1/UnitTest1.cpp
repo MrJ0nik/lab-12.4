5bc09eeb90518fe2b4efc4517b3130d3c0ff6acd
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-11-27 21:40:50 +0200

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,108 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <fstream>
+#include <cstring>
+#include "../lab-12.4/lab-12.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        TEST_METHOD(TestAddPerson)
+        {
+            Note* people = nullptr;
+            int size = 0;
+
+           
+            Note newPerson = { "Ivanov", "Ivan", "1234567890", {1, 1, 1990} };
+            Note* temp = new Note[size + 1];
+            temp[size] = newPerson;
+            delete[] people;
+            people = temp;
+            size++;
+
+            Assert::AreEqual(size, 1);
+            Assert::AreEqual(strcmp(people[0].surname, "Ivanov"), 0);
+            Assert::AreEqual(strcmp(people[0].name, "Ivan"), 0);
+            Assert::AreEqual(strcmp(people[0].phone_number, "1234567890"), 0);
+            Assert::AreEqual(people[0].birthday[0], 1);
+            Assert::AreEqual(people[0].birthday[1], 1);
+            Assert::AreEqual(people[0].birthday[2], 1990);
+
+            delete[] people;
+        }
+
+        TEST_METHOD(TestSortByPhoneNumber)
+        {
+            Note people[3] = {
+                { "Ivanov", "Ivan", "2345678901", {1, 1, 1990} },
+                { "Petrov", "Petr", "1234567890", {15, 5, 1985} },
+                { "Sidorov", "Sidr", "3456789012", {10, 10, 1980} }
+            };
+
+            sortByPhoneNumber(people, 3);
+
+            Assert::AreEqual(strcmp(people[0].phone_number, "1234567890"), 0);
+            Assert::AreEqual(strcmp(people[1].phone_number, "2345678901"), 0);
+            Assert::AreEqual(strcmp(people[2].phone_number, "3456789012"), 0);
+        }
+
+        TEST_METHOD(TestSaveAndLoadFile)
+        {
+            Note people[2] = {
+                { "Ivanov", "Ivan", "1234567890", {1, 1, 1990} },
+                { "Petrov", "Petr", "2345678901", {15, 5, 1985} }
+            };
+
+            int size = 2;
+
+            
+            string filename = "testfile.bin";
+            ofstream outFile(filename, ios::binary);
+            outFile.write(reinterpret_cast<char*>(&size), sizeof(size));
+            outFile.write(reinterpret_cast<char*>(people), sizeof(Note) * size);
+            outFile.close();
+
+            
+            Note* loadedPeople = nullptr;
+            int loadedSize = 0;
+
+            ifstream inFile(filename, ios::binary);
+            inFile.read(reinterpret_cast<char*>(&loadedSize), sizeof(loadedSize));
+            loadedPeople = new Note[loadedSize];
+            inFile.read(reinterpret_cast<char*>(loadedPeople), sizeof(Note) * loadedSize);
+            inFile.close();
+
+            Assert::AreEqual(size, loadedSize);
+            Assert::AreEqual(strcmp(loadedPeople[0].surname, "Ivanov"), 0);
+            Assert::AreEqual(strcmp(loadedPeople[1].surname, "Petrov"), 0);
+
+            delete[] loadedPeople;
+        }
+
+        TEST_METHOD(TestDisplayBySurname)
+        {
+            Note people[2] = {
+                { "Ivanov", "Ivan", "1234567890", {1, 1, 1990} },
+                { "Petrov", "Petr", "2345678901", {15, 5, 1985} }
+            };
+
+            string surname = "Ivanov";
+            bool found = false;
+            for (int i = 0; i < 2; ++i) {
+                if (string(people[i].surname) == surname) {
+                    found = true;
+                    Assert::AreEqual(strcmp(people[i].name, "Ivan"), 0);
+                    Assert::AreEqual(people[i].birthday[0], 1);
+                    Assert::AreEqual(people[i].birthday[1], 1);
+                    Assert::AreEqual(people[i].birthday[2], 1990);
+                    break;
+                }
+            }
+            Assert::IsTrue(found);
+        }
+    };
+}
---
+++ lab-12.4/lab-12.4.cpp
@@ -0,0 +1,184 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip> 
+#include <Windows.h>
+using namespace std;
+
+struct Note {
+    char surname[50];
+    char name[50];
+    char phone_number[20];
+    int birthday[3];
+};
+
+void addPerson(Note*& people, int& size) {
+    Note newPerson;
+    cout << "Введіть прізвище: ";
+    cin.ignore();
+    cin.getline(newPerson.surname, sizeof(newPerson.surname));
+    cout << "Введіть ім'я: ";
+    cin.getline(newPerson.name, sizeof(newPerson.name));
+    cout << "Введіть номер телефону: ";
+    cin.getline(newPerson.phone_number, sizeof(newPerson.phone_number));
+    cout << "Введіть день народження (день, місяць, рік): ";
+    cin >> newPerson.birthday[0] >> newPerson.birthday[1] >> newPerson.birthday[2];
+
+    Note* temp = new Note[size + 1];
+    for (int i = 0; i < size; ++i) {
+        temp[i] = people[i];
+    }
+    temp[size] = newPerson;
+    delete[] people;
+    people = temp;
+    size++;
+}
+
+void sortByPhoneNumber(Note* people, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (string(people[j].phone_number) > string(people[j + 1].phone_number)) {
+                swap(people[j], people[j + 1]);
+            }
+        }
+    }
+    cout << "Масив відсортовано за номером телефону.\n";
+}
+
+void displayBySurname(Note* people, int size, const string& surname) {
+    bool found = false;
+    for (int i = 0; i < size; ++i) {
+        if (string(people[i].surname) == surname) {
+            cout << "Прізвище: " << people[i].surname
+                << ", Ім'я: " << people[i].name
+                << ", Номер телефону: " << people[i].phone_number
+                << ", День народження: " << people[i].birthday[0] << "."
+                << people[i].birthday[1] << "." << people[i].birthday[2] << "\n";
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        cout << "Людина з прізвищем " << surname << " не знайдена.\n";
+    }
+}
+
+void saveToFile(Note* people, int size) {
+    string filename;
+    cout << "Введіть ім'я файлу для збереження: ";
+    cin >> filename;
+
+    ofstream file(filename, ios::out | ios::binary);
+    if (!file) {
+        cout << "Помилка відкриття файлу для запису.\n";
+        return;
+    }
+
+    file.write(reinterpret_cast<char*>(&size), sizeof(size));
+    file.write(reinterpret_cast<char*>(people), sizeof(Note) * size);
+    file.close();
+
+    cout << "Дані збережено у файл.\n";
+}
+
+void loadFromFile(Note*& people, int& size) {
+    string filename;
+    cout << "Введіть ім'я файлу для зчитування: ";
+    cin >> filename;
+
+    ifstream file(filename, ios::in | ios::binary);
+    if (!file) {
+        cout << "Помилка відкриття файлу для зчитування.\n";
+        return;
+    }
+
+    file.read(reinterpret_cast<char*>(&size), sizeof(size));
+    delete[] people;
+    people = new Note[size];
+    file.read(reinterpret_cast<char*>(people), sizeof(Note) * size);
+    file.close();
+
+    cout << "Дані зчитано з файлу.\n";
+}
+
+void displayAllPeople(Note* people, int size) {
+    if (size == 0) {
+        cout << "Список користувачів порожній.\n";
+        return;
+    }
+
+    cout << "+---------------------+----------------------+---------------------+------------------+\n";
+    cout << "| Прізвище            | Ім'я                 | Номер телефону      | Дата народження  |\n";
+    cout << "+---------------------+----------------------+---------------------+------------------+\n";
+
+    for (int i = 0; i < size; ++i) {
+        cout << "| " << setw(20) << left << people[i].surname
+            << "| " << setw(21) << left << people[i].name
+            << "| " << setw(20) << left << people[i].phone_number
+            << "| " << setw(2) << setfill('0') << people[i].birthday[0] << "."
+            << setw(2) << setfill('0') << people[i].birthday[1] << "."
+            << setw(10) << setfill(' ') << people[i].birthday[2] << " |\n";
+    }
+
+    cout << "+---------------------+----------------------+---------------------+------------------+\n";
+}
+
+void displayMenu() {
+    cout << "\n--- Меню ---\n";
+    cout << "1. Додати дані про людину\n";
+    cout << "2. Відсортувати за номером телефону\n";
+    cout << "3. Пошук за прізвищем\n";
+    cout << "4. Зберегти дані у файл\n";
+    cout << "5. Зчитати дані з файлу\n";
+    cout << "6. Вивести всіх користувачів у вигляді таблиці\n";
+    cout << "7. Вийти\n";
+    cout << "Виберіть опцію: ";
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Note* people = nullptr;
+    int size = 0;
+    bool running = true;
+
+    while (running) {
+        displayMenu();
+        int choice;
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            addPerson(people, size);
+            break;
+        case 2:
+            sortByPhoneNumber(people, size);
+            break;
+        case 3: {
+            string surname;
+            cout << "Введіть прізвище для пошуку: ";
+            cin >> surname;
+            displayBySurname(people, size, surname);
+            break;
+        }
+        case 4:
+            saveToFile(people, size);
+            break;
+        case 5:
+            loadFromFile(people, size);
+            break;
+        case 6:
+            displayAllPeople(people, size);
+            break;
+        case 7:
+            running = false;
+            break;
+        default:
+            cout << "Невірний вибір. Спробуйте ще раз.\n";
+        }
+    }
+
+    delete[] people;
+    return 0;
+}
